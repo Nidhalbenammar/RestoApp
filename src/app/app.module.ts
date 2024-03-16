1bd6bcc98bc41b82d56669c596df362621497e3c
Nidhal Ben Ammar <nidhalben13@gmail.com>
2024-03-16 08:03:15 +0100

    Committing local changes before merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -13,14 +12,11 @@ import { AffichageMenuComponent } from './menu/affichage-menu/affichage-menu.com
 import { AddMenuComponent } from './menu/add-menu/add-menu.component';
 import { UpdatePlatComponent } from './menu/update-plat/update-plat.component';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { MenuDetailsComponent } from './menu/menu-details/menu-details.component';
-=======
-import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './users/admin/admin.component';
 import { EtudiantComponent } from './users/etudiant/etudiant.component';
 import { ChefComponent } from './users/chef/chef.component';
->>>>>>> 7e15bcc13c6ebeb80f23f223e1bf9efc58a34162
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,15 +27,12 @@ import { ChefComponent } from './users/chef/chef.component';
     AffichageMenuComponent,
     AddMenuComponent,
     UpdatePlatComponent,
-<<<<<<< HEAD
-    MenuDetailsComponent
-=======
+    MenuDetailsComponent,
     LoginComponent,
     AdminComponent,
     EtudiantComponent,
     ChefComponent
->>>>>>> 7e15bcc13c6ebeb80f23f223e1bf9efc58a34162
-  
+
   ],
   imports: [
     BrowserModule,
