e7caaba540e534e5494d00858484d4a9562c32f7
Nidhal Ben Ammar <nidhalben13@gmail.com>
2024-03-17 13:44:20 +0100

    crud

--- src/app/menu/update-menu/update-menu.component.ts
+++ src/app/menu/update-menu/update-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'; 
 import { MenuService } from '../../services/menu.service';
-import { Menu } from 'src/app/menu';
+import { Menu } from '../models/menu';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
