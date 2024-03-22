bcfbdcbd4c570986ed2a4256b4822da0a6445677
Nidhal Ben Ammar <nidhalben13@gmail.com>
2024-03-22 22:23:36 +0100

    second commit

--- src/app/users/user-routing.module.ts
+++ src/app/users/user-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { ChefComponent } from './chef/chef.component';
 import { EtudiantComponent } from './etudiant/etudiant.component';
+import { etudiantGuard } from './etudiant/guard/etudiant.guard';
+import { chefGuard } from '../menu/guard/chef.guard';
 
 const routes: Routes = [
   {path:"admin",component:AdminComponent},
-  {path:"chef",component:ChefComponent},
-  {path:"etudiant",component:EtudiantComponent}
+  {path:"chef",component:ChefComponent,canActivate:[chefGuard]},
+  {path:"etudiant",component:EtudiantComponent,canActivate:[etudiantGuard]}
   
 ];
 @NgModule({
