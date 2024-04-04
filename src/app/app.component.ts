764b1db8d3aa960bb190f79a61ef2978d6fbe5c3
Rabii khenissi <rabiikhenissi124@gmail.com>
2024-04-04 16:51:57 +0100

    fixNav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'restoapp3';
+  ismenuVisble!:boolean;
+constructor(public auth:AuthService,private router:Router){}
+ngDoCheck(): void {
+  let currentroute=this.router.url;
+  if(currentroute=='/login' || currentroute=='/signup'){
+    this.ismenuVisble=false;
+  }else{
+    this.ismenuVisble=true;
+  }
+  }
+
+  logout(){
+    localStorage.removeItem('jwt');
+    this.router.navigate(['/login']);
+  }
 }
