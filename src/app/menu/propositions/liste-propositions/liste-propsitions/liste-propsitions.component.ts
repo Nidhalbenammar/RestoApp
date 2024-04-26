b204b7a16f8d46a2e6c687f519e35a8978095a13
Nidhal Ben Ammar <nidhalben13@gmail.com>
2024-04-27 00:32:42 +0100

    final

--- src/app/menu/propositions/liste-propositions/liste-propsitions/liste-propsitions.component.ts
+++ src/app/menu/propositions/liste-propositions/liste-propsitions/liste-propsitions.component.ts
@@ -8,24 +8,31 @@ import { OnInit } from '@angular/core';
 })
 export class ListePropsitionsComponent implements OnInit{
   propositions: any[] = [];
-
+  votedPropositions: Set<number> = new Set();
   constructor(private propositionsService: PropositionsService) { }
 
   ngOnInit(): void {
     this.loadPropositions();
+    this.loadStoredVotes();
   }
+
   loadPropositions() {
     this.propositionsService.getAll().subscribe(data => {
       this.propositions = data;
-      this.loadStoredVotes();
     });
   }
 
   vote(proposition: any) {
+    if (this.votedPropositions.has(proposition.id)) {
+      alert('You have already voted for this proposition.');
+      return;
+    }
+
     proposition.vote++;
     this.propositionsService.updateVote(proposition).subscribe(() => {
       localStorage.setItem('vote_' + proposition.id, proposition.vote.toString());
-      this.propositions.sort((a, b) => b.vote - a.vote);
+      this.votedPropositions.add(proposition.id);
+      this.sortPropositions();
     });
   }
 
@@ -34,8 +41,13 @@ export class ListePropsitionsComponent implements OnInit{
       const storedVotes = localStorage.getItem('vote_' + proposition.id);
       if (storedVotes !== null) {
         proposition.vote = parseInt(storedVotes, 10);
+        this.votedPropositions.add(proposition.id);
       }
     });
+    this.sortPropositions();
+  }
+
+  sortPropositions() {
     this.propositions.sort((a, b) => b.vote - a.vote);
   }
 }
